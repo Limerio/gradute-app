6149e484ca3fed93e457627d878b5af5b34b79b6
Limerio <44976231+Limerio@users.noreply.github.com>
2023-10-24 18:57:34 +0200

    Update sidebar layout

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,7 +1,40 @@
+import {
+	SidebarLink,
+	TSidebarLink,
+	UserInfoSidebar,
+} from '@/components/layout/Sidebar'
+import { cn } from '@/utils/functions'
+import { HomeIcon, PlusCircleIcon } from 'lucide-react'
+
 export const Sidebar = () => {
+	const sidebarLinks: TSidebarLink[] = [
+		{
+			href: '/',
+			icon: <HomeIcon />,
+			children: 'Home',
+		},
+		{
+			href: '/notes',
+			icon: <PlusCircleIcon />,
+			children: 'Add notes',
+		},
+	]
+
 	return (
-		<aside className="w-[200px] h-full flex flex-col items-center p-5">
-			Sidebar
+		<aside className={cn('flex flex-col gap-8', 'w-[250px] h-full p-5')}>
+			<UserInfoSidebar />
+
+			<nav className={cn('flex flex-col gap-2')}>
+				<h1 className={cn('text-xl')}>Menu</h1>
+				<div className={cn('flex flex-col gap-5')}>
+					{sidebarLinks.map(sidebarLink => (
+						<SidebarLink
+							key={`sidebar-link-${sidebarLink.href}`}
+							{...sidebarLink}
+						/>
+					))}
+				</div>
+			</nav>
 		</aside>
 	)
 }
