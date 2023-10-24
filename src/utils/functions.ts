7d02d678122db148dab543cc3e1b6aefa7e3672c
Limerio <44976231+Limerio@users.noreply.github.com>
2023-10-24 18:40:16 +0200

    Add getSession function

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+import { clsx, type ClassValue } from 'clsx'
+import { getServerSession } from 'next-auth'
+import { twMerge } from 'tailwind-merge'
+import { authConfig } from './auth-config'
+
+export const cn = (...inputs: ClassValue[]) => {
+	return twMerge(clsx(inputs))
 }
+
+export const getSession = async () => await getServerSession(authConfig)
