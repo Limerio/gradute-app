4c4deca5f11f4ce367c67ee4eb1d61beba72eb92
Limerio <44976231+Limerio@users.noreply.github.com>
2023-10-24 14:00:02 +0200

    Add footer and sidebar

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,3 @@
+export const Footer = () => {
+	return <footer className="p-5 flex items-center">Footer</footer>
+}
---
+++ src/layout/Sidebar.tsx
@@ -0,0 +1,7 @@
+export const Sidebar = () => {
+	return (
+		<aside className="w-[200px] h-full flex flex-col items-center p-5">
+			Sidebar
+		</aside>
+	)
+}
