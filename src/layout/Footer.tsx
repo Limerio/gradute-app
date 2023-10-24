461cfccabdcd5d4261f083da1b9a47f08c4e27fd
Limerio <44976231+Limerio@users.noreply.github.com>
2023-10-24 18:41:19 +0200

    Update Navbar and Footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,3 +1,13 @@
+import { cn } from '@/utils/functions'
+
 export const Footer = () => {
-	return <footer className="p-5 flex items-center">Footer</footer>
+	return (
+		<footer
+			className={cn(
+				'w-full p-5 flex items-center bg-slate-100 dark:bg-slate-800',
+			)}
+		>
+			Footer
+		</footer>
+	)
 }
--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,9 +1,14 @@
 import { ToggleTheme } from '@/components/layout'
+import { cn } from '@/utils/functions'
 
 export const Navbar = () => {
 	return (
-		<nav className="w-full h-8 p-8 flex items-center justify-between">
-			<section>Navbar</section>
+		<nav
+			className={cn(
+				'w-full h-8 p-8 flex items-center justify-between bg-slate-100 dark:bg-slate-800',
+			)}
+		>
+			<section>Graduate</section>
 			<section>
 				<ToggleTheme />
 			</section>
