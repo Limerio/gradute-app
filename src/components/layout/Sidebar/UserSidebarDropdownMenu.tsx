afa593b018cb489a6edb7fe4bd7d40b919b6a6e9
Limerio <44976231+Limerio@users.noreply.github.com>
2023-10-27 18:43:19 +0200

    Update user sidebar dropdown

--- src/components/layout/Sidebar/UserSidebarDropdownMenu.tsx
+++ src/components/layout/Sidebar/UserSidebarDropdownMenu.tsx
@@ -6,11 +6,13 @@ import {
 	DropdownMenuLabel,
 	DropdownMenuSeparator,
 } from '@/components/ui/dropdown-menu'
-import { LogOut } from 'lucide-react'
+import { LogOut, Settings } from 'lucide-react'
 import { signOut } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 import { useCallback } from 'react'
 
 export const UserSidebarDropdownMenu = () => {
+	const router = useRouter()
 	const logout = useCallback(() => {
 		signOut()
 	}, [])
@@ -19,6 +21,10 @@ export const UserSidebarDropdownMenu = () => {
 		<DropdownMenuContent>
 			<DropdownMenuLabel>My Account</DropdownMenuLabel>
 			<DropdownMenuSeparator />
+			<DropdownMenuItem onClick={() => router.push('/settings')}>
+				<Settings className="mr-2 h-4 w-4" />
+				Settings
+			</DropdownMenuItem>
 			<DropdownMenuItem onClick={logout}>
 				<LogOut className="mr-2 h-4 w-4" />
 				Logout
