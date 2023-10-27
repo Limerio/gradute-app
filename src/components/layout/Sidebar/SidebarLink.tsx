e2c32f1b494128d03fc588d7b771122cbff23eba
Limerio <44976231+Limerio@users.noreply.github.com>
2023-10-27 18:33:19 +0200

    Update SidebarLink Component

--- src/components/layout/Sidebar/SidebarLink.tsx
+++ src/components/layout/Sidebar/SidebarLink.tsx
@@ -1,5 +1,5 @@
+import { Link } from '@/components/ui/link'
 import { cn } from '@/utils/functions'
-import Link from 'next/link'
 import { PropsWithChildren, ReactNode } from 'react'
 
 export type TSidebarLink = PropsWithChildren<{
@@ -11,14 +11,14 @@ export const SidebarLink = ({ children, href, icon }: TSidebarLink) => {
 	return (
 		<div
 			className={cn(
-				'py-4 pl-6 pr-12 rounded',
+				'py-4 pl-6 pr-12 rounded-3xl',
 				'hover:cursor-pointer',
 				'bg-slate-100 hover:bg-slate-200',
 				'dark:bg-slate-800 dark:hover:bg-slate-900',
 			)}
 		>
-			<Link href={href} className={cn('flex items-center gap-3')}>
-				{icon} {children}
+			<Link href={href} icon={icon} className={cn('flex items-center gap-3')}>
+				{children}
 			</Link>
 		</div>
 	)
