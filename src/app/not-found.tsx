98d218ed7509b96b8d2881415950541844eba496
Limerio <44976231+Limerio@users.noreply.github.com>
2023-10-27 19:09:27 +0200

    Add error 500 and 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,19 @@
+import { Button } from '@/components/ui/button'
+import Text from '@/components/ui/text'
 import Link from 'next/link'
 
 const NotFound = () => {
 	return (
-		<div>
-			<h2>Not Found</h2>
-			<p>Could not find requested resource</p>
-			<Link href="/">Return Home</Link>
+		<div className="w-full h-full flex flex-col items-center justify-center mx-0 my-auto">
+			<Text as="h1" size="xl">
+				404 Not Found
+			</Text>
+			<Text as="h2" size="lg">
+				Could not find this page
+			</Text>
+			<Button size="lg">
+				<Link href="/">Return Home</Link>
+			</Button>
 		</div>
 	)
 }
